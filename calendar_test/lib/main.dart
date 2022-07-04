f0254f48471e96512f9d32ccc240821574283674
mysend12 <mysend12@naver.com>
2022-07-05 01:03:54 +0900

    exception handler 추가

--- calendar_test/lib/main.dart
+++ calendar_test/lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:calendar_test/calendar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
+  var token = "xoxb-3491253956549-3780327835760-uBKFApeLZdGMVulVQocyDSLi";
+  var url = Uri.parse('https://slack.com/api/chat.postMessage');
+
+  var body = new Map<String, dynamic>();
+  body['channel'] = 'C03MTTK8763';
+
+  var headers = new Map<String, String>();
+  headers['Authorization'] = 'Bearer xoxb-3491253956549-3780327835760-uBKFApeLZdGMVulVQocyDSLi';
+
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    FlutterError.presentError(details);
+    String text = details.stack.toString();
+    body['text'] = text;
+    // await http.post(url, body: body, headers: headers);
+  };
+
   initializeDateFormatting('ko_KR', null);
   runApp(MyApp());
 }
