9e1100aab44a76230218f7ac326ecd57b2e212cb
mysend12 <mysend12@naver.com>
2023-12-24 16:55:54 +0900

    chore: firebase_core 의존성 추가

--- uni_data_flow/macos/Flutter/GeneratedPluginRegistrant.swift
+++ uni_data_flow/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import path_provider_foundation
 import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
