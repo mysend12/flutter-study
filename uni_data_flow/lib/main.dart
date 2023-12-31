48e4eae2efdfcf13fc52cfeb7a009b3717d25ad2
mysend12 <mysend12@naver.com>
2023-12-31 20:39:35 +0900

    feat: firebase token 생성

--- uni_data_flow/lib/main.dart
+++ uni_data_flow/lib/main.dart
@@ -1,6 +1,9 @@
-import 'dart:ui';
+import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -23,10 +26,28 @@ void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
   await dependencyInjection();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.fetchAndActivate();
+  await remoteConfig.setConfigSettings(
+    RemoteConfigSettings(
+      fetchTimeout: const Duration(seconds: 1),
+      minimumFetchInterval: const Duration(hours: 1),
+    ),
+  );
+
+  final firebaseMessaging = FirebaseMessaging.instance;
+
+  String? token;
+  if (kIsWeb) {
+    final vapid = remoteConfig.getString("WEB_PUSH_VAPID");
+    token = await firebaseMessaging.getToken(vapidKey: vapid);
+  } else {
+    token = await firebaseMessaging.getToken();
+  }
 
   runApp(const ProviderScope(child: MyApp()));
 }
